ddf29563912b669f081d33ddcacb3cce37fd9168
mrh960125 <1261565277@qq.com>
2017-06-07 09:45:57 +0800

    删除

--- QingdaoBusinessman/js/index.js
+++
@@ -1,6 +0,0 @@
-$(".myNav ul>li").hover(function(){
-	$(this).children("ul").show().parent("li").siblings("li").children("ul").hide()
-});
-$(".myNav").mouseleave(function(){
-	$(".myNav ul>li>ul").hide()
-})
